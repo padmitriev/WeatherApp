3c8cfbfcd75b4cbeb66c93962f42ef23033b4778
padmitriev <dmkolos@gmail.com>
2025-03-10 06:22:56 +0300

    init add

--- src/main/java/dpa/weather/WeatherApplication.java
+++ src/main/java/dpa/weather/WeatherApplication.java
@@ -2,12 +2,6 @@ package dpa.weather;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.embedded.tomcat.ConfigurableTomcatWebServerFactory;
-import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
-import org.springframework.context.annotation.Bean;
-//import org.h2.tools.Server;
-
-import java.sql.SQLException;
 
 @SpringBootApplication
 public class WeatherApplication {
